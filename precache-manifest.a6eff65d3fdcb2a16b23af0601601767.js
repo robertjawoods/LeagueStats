a2ace665498a480a1f0f97bb07146fb8b21b7b13
Robert Woods <robertjawoods@gmail.com>
2019-03-11 18:20:04 +0000

    Updates

---
+++ precache-manifest.a6eff65d3fdcb2a16b23af0601601767.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee5984c4208076de09a1",
+    "url": "/leaguestats/static/js/runtime~main.e431626b.js"
+  },
+  {
+    "revision": "12761044320b35258d58",
+    "url": "/leaguestats/static/js/main.a4991c09.chunk.js"
+  },
+  {
+    "revision": "6bce3fdbf72c0efe5a2b",
+    "url": "/leaguestats/static/js/2.098369f2.chunk.js"
+  },
+  {
+    "revision": "12761044320b35258d58",
+    "url": "/leaguestats/static/css/main.b4b2ef93.chunk.css"
+  },
+  {
+    "revision": "78b0301b00b90d97deed83129e8752b3",
+    "url": "/leaguestats/index.html"
+  }
+];
