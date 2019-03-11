4639e8013d4ba76c18904f5ff0ba0efd3359cce9
Robert Woods <robertjawoods@gmail.com>
2019-03-11 18:41:35 +0000

    Updates

---
+++ precache-manifest.958d75a4cbe5fc73cc06771e98e7f14a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eed136fdfe2b5240b1ea",
+    "url": "/LeagueStats/static/js/runtime~main.ac39bfea.js"
+  },
+  {
+    "revision": "506c82dc602c32793d62",
+    "url": "/LeagueStats/static/js/main.621bfa78.chunk.js"
+  },
+  {
+    "revision": "6bce3fdbf72c0efe5a2b",
+    "url": "/LeagueStats/static/js/2.098369f2.chunk.js"
+  },
+  {
+    "revision": "506c82dc602c32793d62",
+    "url": "/LeagueStats/static/css/main.b4b2ef93.chunk.css"
+  },
+  {
+    "revision": "b86d84db26ffdd47d74ddb61a6feee65",
+    "url": "/LeagueStats/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/leaguestats/precache-manifest.59c17791eac8ab17f69bacc10a11b2ec.js"
+  "/LeagueStats/precache-manifest.958d75a4cbe5fc73cc06771e98e7f14a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/leaguestats/index.html", {
+workbox.routing.registerNavigationRoute("/LeagueStats/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
