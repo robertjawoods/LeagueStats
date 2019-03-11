e95ed411e091a7eed9cf74c38ebeb826bd6f3340
Robert Woods <robertjawoods@gmail.com>
2019-03-11 18:51:01 +0000

    Fixed routing import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./css/index.css";
-import { Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <Router basename={process.env.PUBLIC_URL}>
