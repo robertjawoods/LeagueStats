d2b078cc580382c4e43c17d5cf3d9c0608f7732e
liamw54 <liamwhite54@gmail.com>
2019-03-23 16:38:47 +0000

    added nav ba

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import "./css/index.css";
+import "./css/main.css";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
